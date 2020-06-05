8c30d91d79c1e0c4a2ab0a7d97574340031e1a49
vanill4Sky <wojtek-s22@wp.pl>
2020-06-05 14:35:20 +0200

    Add handling of images
    Now user can add images as question content or answer content.
    Additionaly the way user selects answers is slightly different.
    Single click works the same way, but there is 150ms delay.
    Double click will open modal window with zoomed image (if selected answer
    is an image).

--- kpz-testownik/src/app/question/question.component.ts
+++ kpz-testownik/src/app/question/question.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
-import { Question, QuestionType } from "../quiz.model";
+import { Question, QuestionType, ContentType } from "../quiz.model";
 import { Subject, Subscription } from "rxjs";
 
 @Component({
   selector: "app-question",
   templateUrl: "./question.component.html",
-  styleUrls: ["./question.component.css"]
+  styleUrls: ["./question.component.css"],
 })
 export class QuestionComponent implements OnInit {
+  ContentType = ContentType;
+
   @Input() question: Question;
   @Input() check: Subject<void> = new Subject<void>();
   @Input() next: Subject<void> = new Subject<void>();
@@ -19,6 +21,9 @@ export class QuestionComponent implements OnInit {
 
   userAnswers: Array<boolean>;
   wasChecked: boolean = false;
+  recentlyClickedAnswer: number;
+  private clickTimer: any;
+  isModalVisible: boolean = false;
 
   private checkSubscription: Subscription;
   private nextSubscription: Subscription;
@@ -42,20 +47,42 @@ export class QuestionComponent implements OnInit {
   }
 
   onAnswerSelect(index: number, value?: boolean): void {
-    switch (this.question.questionType) {
-      case QuestionType.MultipleAnswere:
-        this.userAnswers[index] = !this.userAnswers[index];
-        break;
-      case QuestionType.SingleAnswere:
-        this.userAnswers.fill(undefined);
-        this.userAnswers[index] = true;
-        break;
-      case QuestionType.TrueFalse:
-        this.userAnswers[index] = value;
-        break;
+    this.recentlyClickedAnswer = index;
+
+    if (this.clickTimer) {
+      clearTimeout(this.clickTimer);
     }
-    if (this.userAnswers.some(value => value !== undefined)) {
-      this.answereProvided.emit(true);
+    this.clickTimer = setTimeout(() => {
+      switch (this.question.questionType) {
+        case QuestionType.MultipleAnswere:
+          this.userAnswers[index] = !this.userAnswers[index];
+          break;
+        case QuestionType.SingleAnswere:
+          this.userAnswers.fill(undefined);
+          this.userAnswers[index] = true;
+          break;
+        case QuestionType.TrueFalse:
+          this.userAnswers[index] = value;
+          break;
+      }
+      if (this.userAnswers.some((value) => value !== undefined)) {
+        this.answereProvided.emit(true);
+      }
+    }, 200);
+  }
+
+  showModal(): void {
+    this.isModalVisible = true;
+  }
+
+  hideModal(): void {
+    this.isModalVisible = false;
+  }
+
+  onAnswerContentZoom(index: number): void {
+    clearTimeout(this.clickTimer);
+    if (this.question.answers[this.recentlyClickedAnswer].contentType == ContentType.Image) {
+      this.showModal();
     }
   }
 
@@ -79,7 +106,7 @@ export class QuestionComponent implements OnInit {
         break;
     }
 
-    const isAnswereCorrect = correctChoices.every(value => {
+    const isAnswereCorrect = correctChoices.every((value) => {
       return value;
     });
 
@@ -88,14 +115,12 @@ export class QuestionComponent implements OnInit {
 
   onNext(): void {
     this.wasChecked = false;
-    this.userAnswers = new Array<boolean>(10).fill(
-      undefined
-    );
+    this.userAnswers = new Array<boolean>(10).fill(undefined);
   }
 
   onCheck(): void {
     this.wasChecked = true;
-    this.userAnswers = this.userAnswers.slice(0, this.question.answers.length)
+    this.userAnswers = this.userAnswers.slice(0, this.question.answers.length);
     const isAnswereCorrect = this.isAnswerCorrect(this.question.questionType);
     this.answered.emit(isAnswereCorrect);
   }
